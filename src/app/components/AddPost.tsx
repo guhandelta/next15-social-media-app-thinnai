c0dfd422d52c244b6dda1fdb2c63f59d5951d312
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-06-22 15:14:11 -0400

    Created the Add Posts component

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -3,7 +3,73 @@ import React from 'react'
 
 const AddPost = () => {
   return (
-    <div className="">Add Post</div>
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=800"
+        alt=""
+        width={48}
+        height={48}
+        className='w-12 h-12 rounded-full object-cover'
+        />
+      {/* POST */}
+      <div className=" flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea className='flex-1 p-2 rounded-lg bg-slate-100' placeholder='What is on your mind?'></textarea>
+          <Image 
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className='w-5 h-5 cursor-pointer self-end'
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image 
+              src="/addImage.png"
+              alt=""
+              width={20}
+              height={20}
+              className='w-5 h-5'
+            />
+            <span>Photo</span>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image 
+              src="/addVideo.png"
+              alt=""
+              width={20}
+              height={20}
+              className='w-5 h-5'
+            />
+            <span>Video</span>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image 
+              src="/addEvent.png"
+              alt=""
+              width={20}
+              height={20}
+              className='w-5 h-5'
+            />
+            <span>Event</span>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image 
+              src="/poll.png"
+              alt=""
+              width={20}
+              height={20}
+              className='w-5 h-5'
+            />
+            <span>Poll</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
