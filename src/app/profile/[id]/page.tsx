13f707aa9a861d221a9c2cba6cd5b2bb34263503
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-06-25 00:00:18 -0400

    Added Cover Image and Display Picture to the Profile Page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { LeftMenu, RightMenu } from '@/app/components';
-import { Feed } from '@mui/icons-material';
+import { Feed, LeftMenu, RightMenu } from '@/app/components';
+import Image from 'next/image';
 
 const ProfilePage = () => {
 
@@ -10,6 +10,27 @@ const ProfilePage = () => {
                 </div>
                 <div className="w-full lg:w-[60%] xl:-[50%]">
                     <div className="flex flex-col gap-6">
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="w-full h-64 relative">
+                                <Image src="/cover.png" alt='' fill className='object-cover rounded-md' />
+                                <Image src="/dp.png" alt='' width={128} height={128} className='object-cover w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white' />
+                            </div>
+                            <h1 className="mt-20 mb-4 text-2xl font-medium">Guhaprasaanth Nandagopal</h1>
+                            <div className="flex items-center justify-center gap-12 mb-4">
+                                <div className="flex flex-col items-center">
+                                    <span className="font-medium">231</span>
+                                    <span className="text-sm">Posts</span>
+                                </div>
+                                <div className="flex flex-col items-center">
+                                    <span className="font-medium">1.4k</span>
+                                    <span className="text-sm">Followers</span>
+                                </div>
+                                <div className="flex flex-col items-center">
+                                    <span className="font-medium">632</span>
+                                    <span className="text-sm">Following</span>
+                                </div>
+                            </div>
+                        </div>
                         <Feed />
                     </div>
                 </div>
