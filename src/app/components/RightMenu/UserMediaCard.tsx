404b3a29fd837d655a96582ec9399ab022563b94
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-07-04 12:37:44 -0400

    Updated the UserMedia Card to fetch the media corresponding to the user

--- src/app/components/RightMenu/UserMediaCard.tsx
+++ src/app/components/RightMenu/UserMediaCard.tsx
@@ -1,38 +1,25 @@
-'use client';
-import { User } from "@prisma/client";
+import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link"
-import { useEffect } from "react";
+import prisma from "@/lib/client";
+import { User } from "@prisma/client";
 
-const media = [
-    {
-        id: 1,
-        image: "https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-    {
-        id: 2,
-        image: "https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-    {
-        id: 3,
-        image: "https://images.pexels.com/photos/572861/pexels-photo-572861.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-    {
-        id: 4,
-        image: "https://images.pexels.com/photos/4846437/pexels-photo-4846437.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-    {
-        id: 5,
-        image: "https://images.pexels.com/photos/5560909/pexels-photo-5560909.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-    {
-        id: 6,
-        image: "https://images.pexels.com/photos/784633/pexels-photo-784633.jpeg?auto=compress&cs=tinysrgb&w=800"
-    },
-];
+// 'UserMediaCard' implicitly has type 'any' because it does not have a type annotation and is referenced directly or indirectly in its own initializer, so used te 'FC' type annotation
+const UserMediaCard: FC<{ user: User }> = async ({ user }) => {
 
-const UserMediaCard = ({ user }:{ user : User }) => {
-    media.map(({ id, image }) => console.log({ id, image }));
+    const postsWithMedia = await prisma.post.findMany({
+        where: {
+            userId: user.id,
+            // If only the desc is available, then it is not required
+            img: {
+                not: null
+            },
+        },
+        take: 8,
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
     
     return ( <div className="p-4 bg-white rounded-lg shadow-sm text-sm flex flex-col gap-4">
         {/* TOP */}
@@ -42,14 +29,13 @@ const UserMediaCard = ({ user }:{ user : User }) => {
         </div>
         {/* BOTTOM */}
         <div className="flex flex-wrap gap-4 justify-between">
-            {media.map(({ id, image }) => 
+            {postsWithMedia.length ? postsWithMedia.map(({ id, img }) => 
                 <div key={id} className="relative w-1/5 h-24">
-                    <Image src={image} alt="" fill className="object-cover rounded-md" />
+                    <Image src={img!} alt="" fill className="object-cover rounded-md" />
                 </div>
-            )}
+            ):"No media found!"}
         </div>
     </div>
 )}
-
 
 export default UserMediaCard
